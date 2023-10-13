824792e8e5c9029dc781668f22c01266095fcaab
Ritielen Reis <86447859+ritielenreis@users.noreply.github.com>
2023-10-13 19:13:03 +0100

    Update helpers.py

--- helpers.py
+++ helpers.py
@@ -27,5 +27,3 @@ def deleta_arquivo(id):
     arquivo = recupera_imagem(id)
     if arquivo != 'capa_padrao.jpg':
         os.remove(os.path.join(app.config['UPLOAD_PATH'], arquivo))
-
-deletar = deleta_arquivo(12)
